0fb4037806f434b87935c941121787855c71a757
violetta <violetta.mironchik@gmail.com>
2018-05-17 14:04:53 +0300

    simple flappy bird clone

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var mainState = {
   preload: function() {
-    game.load.image('bird', 'assets/bird.png')
+    game.load.image('bird', 'assets/bird.png');
+    game.load.image('pipe', 'assets/pipe.png');
   },
 
   create: function() {
@@ -9,6 +10,7 @@ var mainState = {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     this.bird = game.add.sprite(100, 245, 'bird');
+    this.pipes = game.add.group();
 
     game.physics.arcade.enable(this.bird);
 
@@ -18,21 +20,61 @@ var mainState = {
       Phaser.Keyboard.SPACEBAR
     );
     spaceKey.onDown.add(this.jump, this);
+
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0",
+      { font: "30px Arial", fill: "#ffffff" });
+
   },
 
   update: function() {
-
     if (this.bird.y < 0 || this.bird.y > 490)
       this.restartGame();
-    },
 
-    jump: function() {
+    game.physics.arcade.overlap(
+      this.bird, this.pipes, this.restartGame, null, this);
+  },
+
+
+  jump: function() {
       this.bird.body.velocity.y = -350;
-    },
+  },
 
-    restartGame: function() {
+  restartGame: function() {
       game.state.start('main');
-    },
+  },
+
+  addOnePipe: function(x, y) {
+
+    var pipe = game.add.sprite(x, y, 'pipe');
+
+    this.pipes.add(pipe);
+
+    game.physics.arcade.enable(pipe);
+
+    pipe.body.velocity.x = -200;
+
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+
+  addRowOfPipes: function() {
+    // Randomly pick a number between 1 and 5
+    // This will be the hole position
+    var hole = Math.floor(Math.random() * 5) + 1;
+
+    // Add the 6 pipes
+    // With one big hole at position 'hole' and 'hole + 1'
+    for (var i = 0; i < 8; i++)
+        if (i != hole && i != hole + 1)
+            this.addOnePipe(400, i * 60 + 10);
+
+    this.score += 1;
+
+    this.labelScore.text = this.score;  
+  },
 };
 
 var game = new Phaser.Game(400, 490);
